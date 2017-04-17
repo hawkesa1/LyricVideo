ba6fe88c95c37b18ae062d5a39d88c3d388c549d
Alex PC <alex@alexhawkes.com>
2017-04-17 18:16:51 +0100

    adding stuff

--- LyricVideo/WebContent/js/animation1.js
+++ LyricVideo/WebContent/js/animation1.js
@@ -7,17 +7,14 @@ function drawIt(ctx, currentAudioTime, lines) {
 	
 	var CURRENT_TIME_POSITION=200;
 	var ZOOM_LEVEL=6;
+	var BACKGROUND_COLOUR='black';
+	var TEXT_COLOUR='white';
 	
-	var lookAheadTime=5;
 	
-	var pixelsPerSecond=(ctx.canvas.width/2)/5;
-	
+	// draw the background
 	ctx.save();
-	ctx.fillStyle = 'yellow'
+	ctx.fillStyle = BACKGROUND_COLOUR;
 	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	ctx.font = "20px Arial";
-	ctx.fillStyle = "red";
-	ctx.fillText(currentAudioTime/1000, 10, 550);
 	ctx.restore();
 	
 	var aLine;
@@ -30,27 +27,25 @@ function drawIt(ctx, currentAudioTime, lines) {
 	for(var i=0; i<lines.length; i++)
 	{
 		aLine=lines[i];
-		if((aLine.startTime < (currentAudioTime +(ZOOM_LEVEL*1000))) && (aLine.endTime > (currentAudioTime)))
+		if((aLine.startTime < (currentAudioTime +(10*1000))) && (aLine.endTime > (currentAudioTime -(2*1000))))
 		{
-			console.log("Current Line="+i);
 			for(var j=0; j<aLine.words.length;j++)
 			{
 				aWord=aLine.words[j];
-				console.log(aWord.word);
-				
 				xPosition=((aWord.startTime-currentAudioTime)/ZOOM_LEVEL)+CURRENT_TIME_POSITION;
-				yPosition=(200 + (j*20));
+				yPosition=(200 + (j*60));
 				wordWidth=(aWord.endTime-aWord.startTime)/ZOOM_LEVEL;
-				
-				console.log(wordWidth)
+
 				 ctx.beginPath();
-				 ctx.rect(xPosition, yPosition, wordWidth, 10);
-				 ctx.font = "20px Arial";
-				 ctx.fillText(aWord.word, xPosition, yPosition-10);
+				 ctx.rect(xPosition, yPosition, wordWidth, 60);
+				 ctx.font = "small-caps 60px Arial";
+				 ctx.strokeStyle = TEXT_COLOUR;
+				 ctx.fillStyle = TEXT_COLOUR;
+				 ctx.fillText(aWord.word, xPosition-5, yPosition-12);
 				 ctx.fillStyle = 'red';
 				 ctx.fill();
-				 ctx.lineWidth = 7;
-				 ctx.strokeStyle = 'black';
+				 ctx.lineWidth = 1;
+				 ctx.strokeStyle = TEXT_COLOUR;
 				 ctx.stroke();
 
 			}	
@@ -66,21 +61,35 @@ function drawIt(ctx, currentAudioTime, lines) {
 	 ctx.rect(CURRENT_TIME_POSITION, 0, 10, ctx.canvas.height);
 	 ctx.fillStyle = 'red';
 	 ctx.fill();
-	 ctx.lineWidth = 7;
-	 ctx.strokeStyle = 'black';
+	 ctx.lineWidth = 1;
+	 ctx.strokeStyle = TEXT_COLOUR;
 	 ctx.stroke();
 
 	 
 	 
 	 ctx.beginPath();
 	 ctx.rect(0, 0, ctx.canvas.width, 100);
-	 ctx.fillStyle = 'red';
+	 ctx.fillStyle = 'white';
 	 ctx.fill();
-	 ctx.lineWidth = 7;
-	 ctx.strokeStyle = 'black';
+	 ctx.lineWidth = 1;
+	 ctx.strokeStyle = TEXT_COLOUR;
 	 ctx.stroke();
 	
-	
+	 ctx.beginPath();
+	 ctx.rect(0, ctx.canvas.height-100, ctx.canvas.width, 100);
+	 ctx.fillStyle = 'white';
+	 ctx.fill(); 
+	 ctx.lineWidth = 1;
+	 ctx.strokeStyle = TEXT_COLOUR;
+	 
+	 
+	 ctx.font = "small-caps 40px Arial";
+	 ctx.strokeStyle = TEXT_COLOUR;
+	 ctx.fillStyle = TEXT_COLOUR;
+	 ctx.fillText("The shit that you promote", 100, ctx.canvas.height-25);
+	 
+	 ctx.stroke();
+	 
 	
 	return ctx;
 }
--- LyricVideo/videoGenerator/generateVideo.bat
+++ LyricVideo/videoGenerator/generateVideo.bat
@@ -11,13 +11,13 @@ SET VIDEO=%ROOT_LOCATION%\videoGenerator\video
 
 @echo %time%
 
-SET FPS=60
+SET FPS=30
 
 MKDIR %GENERATED_FRAMES%
-%PHANTOM_BINARY% %PHANTOM_SCRIPTS%\takeLoadsOfScreenshots.js 1 12000 %GENERATED_FRAMES% %FPS% 800 600
-%FFMPEG_BINARY% -i %GENERATED_FRAMES%image_%%05d.jpg -framerate %FPS% -c:v libx264 -preset slow -crf 1 -pix_fmt yuv420p   -y %VIDEO_SILENT%\out1.mp4
+%PHANTOM_BINARY% %PHANTOM_SCRIPTS%\takeLoadsOfScreenshots.js 1 6000 %GENERATED_FRAMES% %FPS% 1920 1080
+REM %FFMPEG_BINARY% -i %GENERATED_FRAMES%image_%%05d.jpg -framerate %FPS% -c:v libx264 -preset slow -crf 1 -pix_fmt yuv420p   -y %VIDEO_SILENT%\out1.mp4
 %FFMPEG_BINARY% -r %FPS% -i %GENERATED_FRAMES%image_%%05d.jpg -c:v libx264 -preset slow -crf 1 -pix_fmt yuv420p   -y %VIDEO_SILENT%\out1.mp4
-%FFMPEG_BINARY% -r %FPS% -i %VIDEO_SILENT%\out1.mp4 -i %MP3%\dynamite.mp3 -c:v copy -c:a copy -y %VIDEO%\output3.mp4
+%FFMPEG_BINARY% -r %FPS% -i %VIDEO_SILENT%\out1.mp4 -i %MP3%\hardest.mp3 -c:v copy -c:a copy -y %VIDEO%\output3.mp4
 
 DEL %VIDEO_SILENT%\out1.mp4
 RMDIR /S/Q %GENERATED_FRAMES%
